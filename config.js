9f2a6bb0d915bcb25437f0961fe4f59ce5b79ef4
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-07-07 16:38:00 -0500

    updated config.js comments

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 //Use strict
 "use strict";
 
-//exports DATABASE_URL equals process env DATABASE_URL || "mongodb://databaseURL/dbName...";
+//exports DATABASE_URL equals process env DATABASE_URL OR local "mongodb://databaseURL/dbName...";
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blog-app";
 
-//exports TEST_DATABASE_URL equals process env TEST_DATABASE_URL || "mongodb://databaseURL/test_dbName...";
+//exports TEST_DATABASE_URL equals process env TEST_DATABASE_URL  OR local "mongodb://databaseURL/test_dbName...";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/blog-app";
 
 //exports PORT equals process env PORT || <portNum>;
